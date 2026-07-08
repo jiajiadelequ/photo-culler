5b1d49c9863eb4057fdfedd4cf6306175526cf2a
majia1 <majia1@360.com>
2026-07-08 22:50:24 +0800

    fix: bat 文件改用 CRLF 换行符，修复 CMD 解析错误

--- 运行_Qt_源码版.bat
+++ 运行_Qt_源码版.bat
@@ -1,6 +1,6 @@
 @echo off
-cd /d "%~dp0"
-set "TEMP=E:\Temp"
-set "TMP=E:\Temp"
+cd /d %~dp0
+set TEMP=E:\Temp
+set TMP=E:\Temp
 E:\python-envs\photo-culler-qt\Scripts\python.exe photo_culler_qt.py
 pause
